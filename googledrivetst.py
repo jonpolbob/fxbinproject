2029d1a77e4be682168898514d93e71556c694d5
jonpolbob <jonpolbob@gmail.com>
2016-09-26 23:04:54 +0200

    dans googledrivetest : ecriture d'un fichier dans le google drive, dans u sous repertoire et avec un nouveau nom. par ailleurs, peser a couper skype qui pourrit le port 8080 cequi plante le AUth 2.0 (authentification de google API)

--- googledrivetst.py
+++ googledrivetst.py
@@ -1,15 +1,14 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+
+#penser a couper skype !
+#sinon port 8080 est bloqué
+
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
-
-filecsv = drive.CreateFile()
-print("create")
-
-
 
 
 
@@ -19,15 +18,21 @@ fid = ""
 
 file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 for file1 in file_list:
-    print 'title: %s, id: %s' % (file1['title'], file1['id'])
-    if file1['title'] == 'data':
+    print ('title: %s, id: %s' % (file1['title'], file1['id']))
+    if file1['title']== 'data':
         fid = file1['id']
+        print ("found : ",fid)
 #    sys.exit()
 
-f = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": fid}]})
+
+#les bon arguments pour indiquer le id d'un sous repertoire ou ecrire
+# et changer le titre dans le drive
+filecsv = drive.CreateFile({"title":"moncsdetest" , "parents": [{"kind": "drive#fileLink","id": fid}]})
+
+print("create ok")
 
 # ci dessous : le fichier part dans la racine du drive
-filecsv.SetContentFile("c:\\tmp\\DAT_NT_USDCHF_T_BID_201603.csv")
+filecsv.SetContentFile("c:\\tmp\\form1.cs")
 print("setcontent")
 
 
@@ -35,6 +40,8 @@ print("setcontent")
 filecsv.Upload();
 
 print("upload")
+
+print ('Created file %s with mimeType %s' % (filecsv['title'], filecsv['mimeType']))
 
 #files = drive.ListFile()
 
