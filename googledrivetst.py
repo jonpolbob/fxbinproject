c5761bc4f2c53cec7a39774f87c60d94a116c4e7
jonpolbob@gmail.com <jonpolbob@gmail.com>
2016-09-26 17:54:09 +0200

    utilisation de l'auth de l'API google

--- googledrivetst.py
+++ googledrivetst.py
@@ -6,7 +6,37 @@ gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
 
-files = drive.ListFile()
+filecsv = drive.CreateFile()
+print("create")
 
-for x in files:
-    print(x)
+
+
+
+
+#pour ranger le fichier dans un dossier du drive, il faut
+#recuperer un ID du parent et modifier le createfile
+fid = ""
+
+file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+for file1 in file_list:
+    print 'title: %s, id: %s' % (file1['title'], file1['id'])
+    if file1['title'] == 'data':
+        fid = file1['id']
+#    sys.exit()
+
+f = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": fid}]})
+
+# ci dessous : le fichier part dans la racine du drive
+filecsv.SetContentFile("c:\\tmp\\DAT_NT_USDCHF_T_BID_201603.csv")
+print("setcontent")
+
+
+
+filecsv.Upload();
+
+print("upload")
+
+#files = drive.ListFile()
+
+#for x in files:
+#    print(x)
