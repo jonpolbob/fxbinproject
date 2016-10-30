dae51c82e7c0ee90b7d78b27a455a83002a067a3
jonepol <jonpolbob@gmail.com>
2016-10-30 22:51:47 +0100

    ajout de yield mais a refaire car trop complique de les faire par paire

--- readgogdrive.py
+++ readgogdrive.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#readgogdrive : lit un mois sur le drive
+#sauf si le zip BIDmmyy.zip est deja dans c\tmp : dans ce cas : lit le zip du tmp
+
+# 30 oct 16 : ajout de generedata qui lit un paire dans ce zip
+
+
+
 __author__ = 'cagibi'
 
 from pydrive.auth import GoogleAuth
@@ -80,17 +87,126 @@ def decodeline(laligne):
     # print (float(begin))
     return datenumber, float(beginvalue)
 
-
-
+#generedata
+#lit un paire dans le zip
+#et le met dans un tableau
 def generedata(nomzip,paire):
+    letab=[]
     fh1 = open(nomzip, 'rb')
     z1 = zipfile.ZipFile(fh1)  # classe lisant le zipdanzs le fichier ouvert
     with z1.open(paire+".csv", mode='r') as read1:
         for laligne in read1 :
             date,begin = decodeline(laligne)
+            letab.append([date,begin])
             print (date)
-
     fh1.close()
+    return letab
+
+
+def decodetime(time):
+    letuple = time.timetuple()
+
+    day = letuple.tm_day()
+    hour = letuple.tm_hour()
+    minute = letuple.tmmin()
+    weekday = letuple.wday()
+    return day,hour,minute,weekday
+
+
+#bin1Min fait un fichier bin 1 minute avec les 2 tableaux en entree
+def bin1min(year,month,tab1,tab2):
+    currentday=1
+    currenthour=0
+    currentmin=0
+    currenttime = (currentday*24+currenthour)*60+currentmin
+    currentdatime = datetime.datetime(year=year,month=month,day=currentday)
+    currentdayw = currentdatime.timetuple().tm_wday
+
+    time1finish = False
+    time2finish = False
+
+    item1 = tab1.__iter__()
+    item2 = tab2.__iter__()
+    #on commence par un debut de tranche de temps
+    newtime = True; #on est a un debut de tdt
+    hasvalue1 = False #pas encore de valeur pour le 2
+    hasvalue2 = False #pas encore de valeur pour le 1
+    candle1 = []
+
+    deb1=-1
+    max1=-1
+    min1=-1
+    fin1 = -1
+
+    while (True):
+
+        if hasvalue1 == False:
+            deb1 = item1[valeur]
+            fin1 = item1[valeur]
+            max1 = item1[valeur]
+            min1 = item1[valeur]
+            hasvalue1 = True
+
+        fin1 = item1[valeur]
+
+        if max1 < item1[valeur]:
+            max1 = item1[valeur]
+        if min1 > item[valeur]:
+            min1 = item[valeur]
+
+        if hasvalue1 == False:
+           deb2 = item2[valeur]
+           fin2 = item2[valeur]
+           max2 = item2[valeur]
+           min2 = item2[valeur]
+           hasvalue2 = True
+
+        fin2 = item2[valeur]
+        if max2 < item2[valeur]:
+           max2 = item2[valeur]
+        if min2 > item[valeur]:
+          min2 = item[valeur]
+
+        day, hour, minute, weekday = decodetime(item1)
+        if ((day*24+hour)*60+min) > currenttime :  #le temps 1 a depasse le temps courant
+            time1finish=True;
+
+        day, hour, minute, weekday = decodetime(item2)
+        if ((day * 24 + hour) * 60 + min) > currenttime: #le temps 2 a depasse le temps courant
+            time2finish = True;
+
+        #ici on a fini une tranche de temps (qui a ete remplie ou non)
+        if time1finish and time2finish : # timebin finie
+            if hasvalue1: #il y a des valeurs pour 1
+                candle1.append([currenttime,currentdayw,day,hour,minute,deb1,max1,min1,fin1])
+
+            #on passe au temps suivant
+            currentday = 1
+            curenthour = 0
+            currentmin = currentmin+1
+
+            if currentmin == 60:
+                currentmin =0
+                currenthour = currenthour+1
+                if currenthour == 24 :
+                    currenthour =0
+                    currentday = currentday+1
+            currenttime = (currentday * 24 + currenthour) * 60 + currentmin
+
+            newtime=True
+
+
+        if not time2finish:
+            item2 = tab2.__next__()
+
+        if not time1finish:
+            item1 = tab1.__next__()
+
+
+    return
+
+
+
 
 import os.path
 
@@ -109,7 +225,8 @@ else:
     nomfich = readgoogle("2015","10")
 
 #on traite ce fichier (ca l'imprime a l'ecran)
-generedata(nomfich, "EURUSD")
+tab1 = generedata(nomfich, "EURUSD")
+tab2 = generedata(nomfich, "USDCHF")
 
-
+bin1min(tab1,tab2)
 
