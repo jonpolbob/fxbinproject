91685ff2c4fbbc71020f91465e0bf52a1fb182a5
jonpolbob <jonpolbob@gmail.com>
2017-01-23 22:19:48 +0100

    Merge branch 'master' of https://github.com/jonpolbob/fxbinproject essa affichage moy mobile + candles
    # Conflicts:
    #	readgogdrive.py

--- maindetest.py
+++ maindetest.py
@@ -6,9 +6,8 @@ import readweekpaire
 
 import numpy as np
 import pandas as pd
-from matplotlib.finance import candlestick, plot_day_summary, candlestick2
-
-
+from matplotlib.finance import  candlestick_ohlc
+import matplotlib.pyplot as plt
 #34 2015 = 17 aout
 #36 2015 : aout sept
 
@@ -19,29 +18,38 @@ print(tabcandle)
 table=np.zeros(7200)
 for i in tabcandle:
    table[i[0]] = i[1]
-
 print (table)
 
-pdtable = pd.DataFrame(table)
+nantable = table[0:200]
+
+nantable[:]=np.nan
+pdtable = pd.DataFrame(nantable)
+#pdtable = np.nan #init as nan
 length=30
 numsd=2.0
-
-ave = pd.stats.moments.rolling_mean(pdtable,length)
-sd = pd.stats.moments.rolling_std(pdtable,length)
+#ave = pd.stats.moments.rolling_mean(pdtable,length)
+ave = pdtable.rolling(center=False,window=30).mean()
+index = np.arange(0,len(ave))
+#sd = pd.stats.moments.rolling_std(pdtable,length)
+sd = pdtable.rolling(center=False,window=30).std()
 upband = ave + (sd*numsd)
 dnband = ave - (sd*numsd)
 
-print(np.round(ave,3), np.round(upband,3), np.round(dnband,3))
+#tabcurves = np.column_stack([index[30:200],upband[30:200],dnband[30:200]])
+#print (tabcurves[0:200])
+
+#print(np.round(ave,3), np.round(upband,3), np.round(dnband,3))
 
 # Plot two charts to assess trades and equity curve
 fig = plt.figure()
 fig.patch.set_facecolor('white')  # Set the outer colour to white
 ax1 = fig.add_subplot(211, ylabel='Price in $')
 
-# Plot the "buy" trades against AAPL
-ax1.plot(upband,'^', markersize=10, color='m')
-ax1.plot(upband,'^', markersize=10, color='m')
-
-candlestick(ax1, table, width=0.6)
-fig.shw()
+# Plot the "buy" trades against
+#ax1.plot(upband,'^', markersize=10, color)
+#ax1.plot(upband,'^', markersize=10, color='m'
+candlestick_ohlc(ax1, tabcandle, width=.8)
+ax1.plot(index,upband,'^', ls='-', markersize=1, color='m')
+ax1.plot(index,dnband,'^', ls='-', markersize=1, color='g')
+plt.show()
 
