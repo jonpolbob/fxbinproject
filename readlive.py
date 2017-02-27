e4c286ac955536ffff1146852dc75e1138c2a8d2
jonpolbob@gmail.com <jonpolbob@gmail.com>
2017-02-27 17:38:55 +0100

    essai detection des valeurs dans page live acvec xpath

--- readlive.py
+++ readlive.py
@@ -3,11 +3,49 @@ from lxml import etree
 import requests
 
 page = requests.get('https://www.dailyfx.com/forex-rates')
-tree = html.fromstring(page.content)
+#for e in page:
+#    print(e)
 
-eurusdvalue = tree.xpath('//tr[@id="EURUSD"]') #execute une instruction xpath pour trouve le truc
-eurusdchild = eurusdvalue.getchildren()
-print(eurusdchild)
+
+tree = html.fromstring(page.content)
+truc = tree.xpath("//span[@class]")
+
+
+#eurusdvalue = tree.xpath('//tr[@id="EURUSD"]') #execute une instruction xpath pour trouve le truc
+#lestd = eurusdvalue[0].xpath('//td/b/span[@id="eurusd-priceBid"]')
+eurusdvalue2=None
+eurusdvalue = tree.xpath('//div')
+for e in eurusdvalue:
+    if ('class' in e.attrib):
+        if (e.attrib['class'] == 'col-sm-6'):
+            eurusdvalue2 = e.xpath('//table/tbody/tr')#    print (e.attrib)
+            break
+
+eurusdvalue3 = None
+
+if (eurusdvalue2 != None):
+    for e in eurusdvalue2:
+        if ('id' in e.attrib):
+            if e.attrib['id'] == 'EURUSD0':
+                #print(e.attrib['id'])
+                eurusdvalue3= e.xpath('//td/a/div')
+
+eurusdvalue4 = None
+
+if (eurusdvalue3 != None):
+    for e in eurusdvalue3:
+        eurusdvalue4 = eurusdvalue3.xpath('//div/div')
+        for z in eurusdvalue4:
+            if 'id' in z.attrib:
+                print(z.attrib['id'])
+                if ('priceLow' in z.attrib['id']):
+                    print(z.attrib['id'][:6],':',z.text)
+                    print (z.xPath("//text()"))
+
+                #if (e.attrib['class'] == 'container'):
+                #eurusdvalue2 = e.xpath('//tr')  # print (e.attrib)
+
+        #print (e.attrib)
 
 usdjpyvalue = tree.xpath('//tr[@id="USDJPY"]')
 print(usdjpyvalue)
